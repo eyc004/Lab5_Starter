d4e8f635def044a8dd1a1e2407c11da49b1f27f6
ericy <eyc004@ucsd.edu>
2022-10-30 17:10:20 -0700

    Figured out how to do correct image displays

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,17 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let horn_type = document.getElementById('horn-select');
+  horn_type.addEventListener('change', (event) => {
+    if (horn_type.value == 'air-horn') {
+      document.querySelector("img").src = 'assets/images/air-horn.svg'
+    }
+    if (horn_type.value == 'car-horn') {
+      document.querySelector("img").src = 'assets/images/car-horn.svg'
+    }
+    if (horn_type.value == 'party-horn') {
+      document.querySelector("img").src = 'assets/images/party-horn.svg'
+    }
+  });
+  
 }
